19ffc61678194e50a7c2c423f074cf9e35e460ae
Jugal <kumawatjugal1998@gmail.com>
2023-06-28 09:51:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryItem from './components/category-tems/category-item.component';
+import './categories.styles.scss';
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl:'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl:'https://i.ibb.co/GCCdy8t/women.png',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl:'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map((category)=>(
+        <CategoryItem Key ={category.id} category={category}/>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
